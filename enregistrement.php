1ac1ad5bab86201b1ef1146ce3173d4a6148c3af
Camara-Diadie <45995527+Camara-Diadie@users.noreply.github.com>
2019-12-31 02:28:39 +0100

    realitation du code php connection bdd et enregistrement

--- enregistrement.php
+++ enregistrement.php
@@ -1,3 +1,37 @@
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=gbaf', 'root','');
+}
+catch(Exception $e)
+{
+    die('erreur:'.$e->getMessage());
+}
+ if ( isset ($_POST['teste']))
+{
+    if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['speudo']) AND !empty($_POST['mdp']) AND !empty($_POST['question']) AND !empty($_POST['secret']))
+    {
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $speudo = htmlspecialchars($_POST['speudo']);
+        $mdp = sha1($_POST['mdp']);
+        $question= htmlspecialchars($_POST['question']);
+        $secret = sha1($_POST['secret']);
+    }
+
+    
+
+}
+
+    $insertacteur = $bdd->prepare('INSERT INTO acteur_utilisateur(nom, prenom, nom_utilisateur, mot_de_pass, question, reponse) VALUES (?, ?, ?, ?, ?, ?)');
+    $insertacteur->execute(array($nom, $prenom, $speudo, $mdp, $question, $secret));
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -18,19 +52,19 @@
 
     <section class="formulaire">
         <div class="formulaire-enregistrement">
-            <form >
+            <form method="POST" action="">
                 <h1>Création de compte GBAF</h1>
                 <label for="nom">Nom : </label>
                 <input type="text" placeholder="Entrer votre Nom" name="nom" id="nom" required><br>
                 <label for="prenom">Prénom : </label>
                 <input type="text" placeholder="Entrer votre Prénom" name="prenom" id="prenom" required><br>
                 <label for="utilisateur">Nom d'utilisateur : </label>
-                <input type="text" placeholder="Nom d'utilisateur" name="username" id="utilisateur" required></br>
+                <input type="text" placeholder="Nom d'utilisateur" name="speudo" id="utilisateur" required></br>
                 <label for="mot-de-passe"> Mot de passe :</label>
-                <input type="password" placeholder="votre Mdp" name="UserPassword" id="mot-de-passe" required><br>
-                <label for="secret">Qestion secrèt:<br> Quelle est votre couleur préférée? </label>
+                <input type="password" placeholder="votre Mdp" name="mdp" id="mot-de-passe" required><br>
+                <label for="question">Qestion secret <SELECT name="question" size="0"><OPTION>le nom de votre animal<OPTION>le nom de votre meilleur ami(e)</SELECT></label>
                 <input type="text" placeholder="Réponse" name="secret" id="secret" required><br>
-                <input type="submit"  id="submit" value="Enregistré">
+                <input type="submit"  id="submit" name="teste" value="Enregistré">
             </form>
         </div>
     </section>
